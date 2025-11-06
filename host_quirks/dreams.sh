1a2a4d207eeab199271151618f086f8a68e5c608
lucdev <me@lucdev.net>
2025-11-06 17:18:00 -0300

    rename laptop script

---
+++ host_quirks/dreams.sh
@@ -0,0 +1,7 @@
+# For node-gyp to work
+export RPM_ARCH=$(uname -m)
+
+# Playwright install location
+export PLAYWRIGHT_BROWSERS_PATH="$HOME/.local/opt/playwright"
+
+
--- host_quirks/lucs-dev-laptop.sh
+++
@@ -1,2 +0,0 @@
-# For node-gyp to work
-export RPM_ARCH=$(uname -m)
